0bfefabd396a8e4dadbfb56b406d3eb991704420
erik-vm <60032776+erik-vm@users.noreply.github.com>
2023-03-16 22:38:10 +0200

    Initial commit

---
+++ src/main/java/vm/erik/order_management_system_api/OrderManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package vm.erik.order_management_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagementSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vm/erik/order_management_system_api/OrderManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vm.erik.order_management_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
